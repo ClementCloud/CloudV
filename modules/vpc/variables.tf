1417db0aaa8f349b168dcb5917222c0ba71fdb9d
clementCloud <clefami@gmail.com>
2024-01-17 22:07:50 -0500

    futher build out of the varibles and locals file

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -2,7 +2,9 @@ variable "project_name" {
   type = string
   default = "cloudV"
 }
-
+variable "region" {
+  type = string
+}
 variable "environment" {
   type = string
 }
@@ -10,4 +12,23 @@ variable "environment" {
 variable "default_tags" {
   type = map(string)
   default = {}
+}
+variable "vpc_cidr_block" {
+  type    = string
+  description = "The CIDR block for the VPC"
+}
+
+variable "subnet_cidr_blocks" {
+  type    = list(string)
+  description = "The CIDR blocks for the subnets"
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  description = "The availability zones for the subnets"
+}
+
+variable "security_group_ids" {
+  type    = list(string)
+  description = "The IDs of the security groups to associate with the VPC"
 }
